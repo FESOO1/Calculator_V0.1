b3064ae3e5b5209d7ac757639dd157cbcc899d2f
FES001 <firdavs7794948@gmail.com>
2025-02-20 20:33:29 +0500

    Working on it.

---
+++ script.js
@@ -0,0 +1,5 @@
+// CALCULATOR SCREEN
+const calculatorScreen = document.querySelector('.calculator-middle');
+
+// CALCULATOR NUMBER BUTTONS
+const calculatorButtonNumber = document.querySelectorAll('.calculator-bottom-button-number');
