cc5ae6ef34357f14cdd1734996cc958aaba6bda9
FES001 <firdavs7794948@gmail.com>
2025-02-20 21:13:57 +0500

    Working on it.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // CALCULATOR SCREEN
-const calculatorScreen = document.querySelector('.calculator-middle');
+const calculatorScreen = document.querySelector('.calculator-middle-screen');
 
 // CALCULATOR NUMBER BUTTONS
 const calculatorButtonNumber = document.querySelectorAll('.calculator-bottom-button-number');
@@ -15,3 +15,57 @@ const calculatorCalculateButton = document.querySelector('.calculator-bottom-but
 
 // CALCULATOR MATH OPERATOR BUTTON
 const calculatorMathOperatorButton = document.querySelectorAll('.calculator-bottom-button-math-operator');
+
+// CALCULATOR
+let calculator = {
+    calculatorFirstEnteredDigits: [],
+    calculatorSecondEnteredDigits: [],
+    isClickedOnMathOperator: false,
+    ClickedOnMathOperator: '',
+};
+
+// ENTERING DIGITS
+
+for (let i = 0; i < calculatorButtonNumber.length; i++) {
+    calculatorButtonNumber[i].addEventListener('click', () => {
+        if (calculator.isClickedOnMathOperator === false) {
+            calculator.calculatorFirstEnteredDigits.push(calculatorButtonNumber[i].value);
+            
+            // SHOWING THE CLICKED DIGIT ON THE SCREEN
+            const beingEnteredDigitString = calculator.calculatorFirstEnteredDigits.join(',');
+            calculatorScreen.innerHTML = beingEnteredDigitString.replaceAll(',', '');
+        } else {
+            calculator.calculatorSecondEnteredDigits.push(calculatorButtonNumber[i].value);
+
+            // SHOWING THE CLICKED DIGIT ON THE SCREEN
+            const beingEnteredDigitString = calculator.calculatorSecondEnteredDigits.join(',');
+            calculatorScreen.innerHTML = beingEnteredDigitString.replaceAll(',', '');
+        };
+
+        console.log(calculator.calculatorFirstEnteredDigits);
+    });
+};
+
+// DELETING A DIGIT
+
+function deletingOneDigit() {
+    if (calculator.isClickedOnMathOperator === false) {
+        calculator.calculatorFirstEnteredDigits.pop();
+        const beingEnteredDigitString = calculator.calculatorFirstEnteredDigits.join(',');
+        calculatorScreen.innerHTML = beingEnteredDigitString.replaceAll(',', '');
+    } else {
+        calculator.calculatorSecondEnteredDigits.pop();
+        const beingEnteredDigitString = calculator.calculatorSecondEnteredDigits.join(',');
+        calculatorScreen.innerHTML = beingEnteredDigitString.replaceAll(',', '');
+    };
+};
+
+// MATH OPERATOR
+for (let i = 0; i < calculatorMathOperatorButton.length; i++) {
+    calculatorMathOperatorButton[i].addEventListener('click', () => {
+
+    });
+};
+
+// INITIALIZING BUTTONS
+calculatorDeleteButton.addEventListener('click', deletingOneDigit);
