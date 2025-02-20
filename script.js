883e0ac831ed4b05eca5ac62bc5019349045b0bf
FES001 <firdavs7794948@gmail.com>
2025-02-20 20:38:48 +0500

    Workin gon it.

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const calculatorScreen = document.querySelector('.calculator-middle');
 
 // CALCULATOR NUMBER BUTTONS
 const calculatorButtonNumber = document.querySelectorAll('.calculator-bottom-button-number');
+
+// CALCULATOR DELETE BUTTON
+const calculatorDeleteButton = document.querySelector('.calculator-bottom-button-delete');
+
+// CALCULATOR RESET BUTTON
+const calculatorResetButton = document.querySelector('.calculator-bottom-button-reset');
+
+// CALCULATOR CALCULATE BUTTON
+const calculatorCalculateButton = document.querySelector('.calculator-bottom-button-equal');
+
+// CALCULATOR MATH OPERATOR BUTTON
+const calculatorMathOperatorButton = document.querySelectorAll('.calculator-bottom-button-math-operator');
